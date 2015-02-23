1fef782bdee18c767c4d12150f4f254d97d6d956
Josh Grancell <jgrancell@campbellmarketing.com>
2015-02-23 17:00:54 -0500

    Updated notifications, removed .dat files.

--- cron_update.sh
+++ cron_update.sh
@@ -9,13 +9,9 @@ cd "$TEMP"
 
 wget -q https://www.rfxn.com/downloads/rfxn.hdb
 wget -q https://www.rfxn.com/downloads/rfxn.ndb
-wget -q https://www.rfxn.com/downloads/hex.dat
-wget -q https://www.rfxn.com/downloads/md5.dat
 
 cat rfxn.hdb > /usr/local/share/clamav/rfxn.hdb
 cat rfxn.ndb > /usr/local/share/clamav/rfxn.ndb
-cat hex.dat > /usr/local/share/clamav/hex.dat
-cat md5.dat > /usr/local/share/clamav/md5.dat
 
 DATE=$(date)
 
--- malscan.sh
+++ malscan.sh
@@ -2,16 +2,13 @@
 # Malscan - Enhanced ClamAV Scanning System 
 # Written by Josh Grancell
 
-VERSION="1.2.0"
+VERSION="1.2.1"
 DATE="Feb 23 2015"
 
-MAINDIR="/usr/local/share/clamav"
-
+MAINDIR="/var/lib/clamav"
 LOGDIR="$MAINDIR/log"
-DETECTIONLOG="$LOGDIR/detection.log"
 QDIR="$MAINDIR/quarantine"
-TEMPDIR=$(mktemp -d)
-TEMPLOG="$TEMPDIR/malscan.log"
+
 CLAMSCAN=$(which clamscan)
 
 if [[ $# -lt "1" || "$1" == "-h" || "$1" == "--version" ]]; then
@@ -40,11 +37,11 @@ if [[ "$1" == "-r"|| "$1" == "--report" ]]; then
 	sigtool --md5 "$2" >> "$MAINDIR"/custom.hdb
 fi
 
-echo -ne "\033[31m"
-"$CLAMSCAN" -d "$MAINDIR"/rfxn.hdb -d "$MAINDIR"/rfxn.ndb -d "$MAINDIR"/custom.hdb -d "$MAINDIR"/custom.ndb -i -r --no-summary --exclude='quarantine' "$1" | tee "$TEMPLOG"
-echo -ne "\033[37m"
+SCANLOG=$LOGDIR/$(date +%F-%s)
 
-echo "$DATE" >> /usr/local/share/clamav/log/scan.log
+echo -ne "\033[31m"
+"$CLAMSCAN" -d "$MAINDIR"/rfxn.hdb -d "$MAINDIR"/rfxn.ndb -d "$MAINDIR"/custom.hdb -d "$MAINDIR"/custom.ndb -i -r --no-summary --exclude='quarantine' "$1" | tee "$SCANLOG"
+echo -ne "\033[37m"
 
 if [[ -n "$QUARANTINE" ]]; then
 	while read -r; do
@@ -55,37 +52,33 @@ if [[ -n "$QUARANTINE" ]]; then
 		mv "$ABSPATH" "$QDIR""$ABSPATH"
 		rsync -avzP "$QDIR"/ -e ssh /home/jgrancell/quarantine/"$HOSTNAME"/
 		chmod 000 "$QDIR""$ABSPATH"
-		echo -e "\033[36m$FILE quarantined in $QDIR"
-	done < <(grep -v "globals" "$TEMPLOG" | cut -d: -f1)
+		echo -e "\033[36m$FILE quarantined in $QDIR" >> $LOGDIR/quarantine.log
+	done < <(grep -v "globals" "$SCANLOG" | cut -d: -f1)
 
 	while read -r; do
 		ABSPATH=$(readlink -f "$REPLY")
 		DIR=$(dirname "$ABSPATH")
 		FILE=$(basename "$ABSPATH")
-		echo -e "\033[35m$FILE has been flagged as suspicious, but not quarantined." >> $DETECTIONLOG
-	done < <(grep "globals" "$TEMPLOG" | cut -d: -f1)
+		echo -e "\033[35m$FILE has been flagged as suspicious, but not quarantined." >> $LOGDIR/quarantine.log
+	done < <(grep "globals" "$SCANLOG" | cut -d: -f1)
 fi
 
 ## Setting Up The Alert Email
-if [ -f "$TEMPLOG" ]; then
+if [ -s "$SCANLOG" ]; then
 	EMAIL_TMP=$(mktemp)
 	{
 	echo "To:jgrancell@campbellmarketing.com"
 	echo "From:automated-malscan-service@campbellmarketing.services"
-	echo "Subject: $HOSTNAME Malware Scanner Test 4"
+	echo "Subject: Malware Detections: $HOSTNAME - $(date)" 
 	echo "MIME-Version: 1.0"
 	echo "Content-Type: text/html; charset="us-ascii" "
 	echo "Content-Disposition: inline"
 	echo "<!DOCTYPE html>"
-	echo "<html> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"
-	echo "<body>$(cat $TEMPLOG) </body></html>" 
-	} >> $EMAIL_TMP
+	echo "<html> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"	
+	echo "<body> Malicious and/or suspicious files have been identified on $HOSTNAME. Please see $SCANLOG for further information. </body></html>"
+	} >> "$EMAIL_TMP"
 
-
-
-	sendmail -i -t < $EMAIL_TMP
+	sendmail -i -t < "$EMAIL_TMP"
 fi
-
-rm -rf "$TEMPLOG"
 
 exit 0
