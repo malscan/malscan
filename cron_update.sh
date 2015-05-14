5adc402faee18c18c78e2a9e4bc0df24207a8dfc
Josh Grancell <josh@joshgrancell.com>
2015-05-14 15:12:39 -0400

    Corrected conf file path
    Made the cron script intelligent enough to know where the conf.malscan file is.

--- cron_update.sh
+++ cron_update.sh
@@ -3,7 +3,17 @@
 # Written by Josh Grancell
 # Since: 1.0
 
-source conf.malscan
+## Identifying where we're running the script from
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+## Loading the configuration file from the Malscan directory
+source /"$DIR"/"conf.malscan"
 
 TEMP=$(mktemp -d)
 cd "$TEMP"
