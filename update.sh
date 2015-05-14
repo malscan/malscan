313162d9804921ad94052749172c50b60454d263
Josh Grancell <josh@joshgrancell.com>
2015-05-14 15:49:13 -0400

    Renamed and Updated
    Renamed the cron_update.sh file to update.sh, to simply the naming convention for files
    Updated the new update.sh file to correct several $MAINDIR and $USER references to $MALSCAN_DIRECTORY and $MALSCAN_USER, respectively

--- update.sh
+++ update.sh
@@ -25,34 +25,34 @@ wget -q https://www.rfxn.com/downloads/rfxn.ndb
 wget -q https://repo.joshgrancell.com/custom.hdb
 wget -q https://repo.joshgrancell.com/custom.ndb
 
-cat rfxn.hdb > "$MAINDIR"/rfxn.hdb
-cat rfxn.ndb > "$MAINDIR"/rfxn.ndb
-cat custom.hdb > "$MAINDIR"/custom.hdb
-cat custom.ndb > "$MAINDIR"/custom.ndb
+cat rfxn.hdb > "$MALSCAN_DIRECTORY"/rfxn.hdb
+cat rfxn.ndb > "$MALSCAN_DIRECTORY"/rfxn.ndb
+cat custom.hdb > "$MALSCAN_DIRECTORY"/custom.hdb
+cat custom.ndb > "$MALSCAN_DIRECTORY"/custom.ndb
 
 echo "Running the freshclam updater."
 "$FRESHCLAM_BINARY_LOCATION"
 
 DATE=$(date)
 
-if [[ ! -d "$MAINDIR"/log ]]; then
-	mkdir "$MAINDIR"/log
+if [[ ! -d "$MALSCAN_DIRECTORY"/log ]]; then
+	mkdir "$MALSCAN_DIRECTORY"/log
 fi
 
-if [[ ! -d "$MAINDIR"/quarantine ]]; then
-	mkdir "$MAINDIR"/quarantine
+if [[ ! -d "$MALSCAN_DIRECTORY"/quarantine ]]; then
+	mkdir "$MALSCAN_DIRECTORY"/quarantine
 fi
 
 if [[ ! -h "$MALSCAN_BINARY_LOCATION" ]]; then
-	ln -s "$MAINDIR"/malscan.sh $MALSCAN_BINARY_LOCATION
+	ln -s "$MALSCAN_DIRECTORY"/malscan.sh $MALSCAN_BINARY_LOCATION
 fi
 
 echo "Cleaning up..."
 
-echo "$DATE" >> "$MAINDIR"/log/update.log
+echo "$DATE" >> "$MALSCAN_DIRECTORY"/log/update.log
 
 rm -rf "$TEMP"
 
-chown "$USER":"$USER" "$MAINDIR" -R
+chown "$MALSCAN_USER":"$MALSCAN_USER" "$MALSCAN_DIRECTORY" -R
 
 exit 0
