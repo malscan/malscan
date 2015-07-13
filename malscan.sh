eb72c7444fb9c2f49b396a6b3f38983e74e6909a
Josh Grancell <josh@joshgrancell.com>
2015-07-13 15:12:20 -0400

    Incremented version number

--- malscan.sh
+++ malscan.sh
@@ -2,8 +2,8 @@
 # Malscan - Enhanced ClamAV Scanning System
 # Written by Josh Grancell
 
-VERSION="1.5.1"
-DATE="June 16 2015"
+VERSION="1.5.2"
+DATE="July 13 2015"
 
 ## Identifying where we're running the script from
 SOURCE="${BASH_SOURCE[0]}"
