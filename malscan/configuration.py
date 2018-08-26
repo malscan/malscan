f3ae4b6106390639d83adc63176e3d93b524eb31
Josh Grancell <jgrancell@malscan.com>
2018-08-25 22:25:12 -0400

    Refactored code

--- malscan/configuration.py
+++ malscan/configuration.py
@@ -10,6 +10,7 @@
 # --------------------------------------------------
 from os import path
 import sys
+import json
 from malscan.error import Error
 
 
@@ -54,7 +55,7 @@ class Configuration:
         self.global_config = self.base_path + "/config/malscan.conf"
 
         if path.isfile(self.global_config):
-            self._load_config_file()
+            self._load_configuration_file()
         else:
             Exception.error(
                 "The malscan configuration file "
@@ -63,7 +64,7 @@ class Configuration:
             sys.exit(1)
 
         if path.isfile(self.local_config):
-            self._load_config_file('local')
+            self._load_configuration_file('local')
             self.configuration_file = self.local_config
             self.configuration_mode = 'local'
         else:
@@ -76,23 +77,57 @@ class Configuration:
         return self.configuration[config_option]
 
     def show(self):
-        print(self.configuration)
+        json.dump(self.configuration, sys.stdout, indent=4)
+        print("")
 
-    def _load_config_file(self, locale='global'):
-        # Saving configuration values
+    def _load_configuration_file(self, locale='global'):
         if locale == 'local':
-            config_file = self.local_config
+            configuration_file = self.local_config
         else:
-            config_file = self.global_config
+            configuration_file = self.global_config
 
-        file = open(config_file, 'r')
+        file = open(configuration_file, 'r')
         for line in file.readlines():
-            if line[0] != "#":
-                option = str.strip(line).split()
-                if option != []:
-                    if self._sanity_check(option[0], option[1], locale):
-                        self.configuration[option[0]] = option[1]
-        file.close()
+            self._parse_file_line(line, locale)
+
+    def _parse_file_line(self, line, locale):
+        if line[0] == '#':
+            return None
+        else:
+            option = str.strip(line).split()
+            if option != []:
+                key = option[0]
+                value = option[1]
+                self._save_configuration_option(key, value, locale)
+
+    def _save_configuration_option(self, key, value, locale):
+        if self._validate_configuration_option(key, value, locale):
+            self.configuration[key] = value
+
+    def _validate_configuration_option(self, key, value, locale):
+        if key in self.config_options:
+            if self.config_options[key] == []:
+                return True
+            elif value in self.config_options[key]:
+                return True
+            else:
+                config_values = '\n'.join(self.config_options[key])
+                warning = (
+                    "The value {} cannot be set for the configuration "
+                    "option {}. Available configuration values: {}"
+                ).format(value, key, config_values)
+
+                if locale == 'local':
+                    self.exception.warning(warning)
+                else:
+                    self.exception.error(warning)
+                return False
+        else:
+            self.exception.warning(
+                "The option {} does not exist "
+                "and could not be set.".format(key)
+            )
+            return False
 
     # Sanity checks Malscan configuration options
     def _sanity_check(self, option, value, locale):
